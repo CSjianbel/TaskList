1c4d5c2df53e61a0a2498567828b9fc2dd065800
CSjianbel <belarminojiankarlo@gmail.com>
2024-03-27 13:56:13 +0800

    feat: added user model

---
+++ server/models/user.model.js
@@ -0,0 +1,21 @@
+const user = (sequelize, Sequelize) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  });
+  return User;
+};
+
+export default user;
