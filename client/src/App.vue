cd6917ecad9687eecdb642565cbf8b2f16ce5e5a
CSjianbel <belarminojiankarlo@gmail.com>
2024-03-27 11:48:29 +0800

    feat: added base table

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,12 +5,32 @@ import AddTask from './components/AddTask.vue';
 
 
 <template>
-  <div class="flex flex-col justify-center items-center p-4">
-    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Task List</span></h1>
+  <div class="flex flex-col justify-center items-center p-4 space-x-5 space-y-5">
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span
+        class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Task List</span></h1>
 
     <AddTask />
+
+
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            <th scope="col" class="px-6 py-3">
+              Task
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div>
+
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
