2b071baca42483c15dfc28548dcc64899f64e58b
CSjianbel <belarminojiankarlo@gmail.com>
2024-03-27 11:34:56 +0800

    feat: Initialized Project

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AddTask from './components/AddTask.vue';
+</script>
+
+
+
+<template>
+  <div class="flex flex-col justify-center items-center p-4">
+    <h1 class="text-lg">Hello, World!</h1>
+    <AddTask />
+
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ client/src/components/AddTask.vue
@@ -0,0 +1,17 @@
+<template>
+  <form>
+    <div class="flex space-x-4">
+      <input type="text" id="task" name="task" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="safdstudy vue" required />
+      <button type="submit" class="inline-block px-8 py-2 text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:ring-4 focus:outline-none">Add Task</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
+<!--  -->
