3653b2fe4b68144bba0854b3d0bb6b9d081f368b
CSjianbel <belarminojiankarlo@gmail.com>
2024-03-27 20:58:28 +0800

    feat: added not found page

--- client/src/views/NotFound/NotFoundPage.vue
+++ client/src/views/NotFound/NotFoundPage.vue
@@ -14,12 +14,12 @@
         </p>
         <p class="mb-4 text-lg font-light text-gray-500">
           Sorry, we can't find that page. You'll find lots to explore on the
-          home page.
+          tasks list page.
         </p>
         <router-link
           class="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-        >
-          to="/" >Back to Tasks List Page</router-link
+          to="/"
+          >Back to Tasks List Page</router-link
         >
       </div>
     </div>
