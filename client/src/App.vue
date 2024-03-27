ffd8e4385607a2c0ee37193b60c732e4a3226324
CSjianbel <belarminojiankarlo@gmail.com>
2024-03-27 11:39:45 +0800

    feat: added styling to header and add task component

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,9 +6,9 @@ import AddTask from './components/AddTask.vue';
 
 <template>
   <div class="flex flex-col justify-center items-center p-4">
-    <h1 class="text-lg">Hello, World!</h1>
-    <AddTask />
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Task List</span></h1>
 
+    <AddTask />
   </div>
 </template>
 
